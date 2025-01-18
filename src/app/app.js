56883f6f256ce8e95c14e373e2af83236652b9d4
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-01-18 00:15:17 -0300

    feat(App): Added example of generic and specific middleware

--- src/app/app.js
+++ src/app/app.js
@@ -7,6 +7,23 @@ await databaseConnect();
 
 const app = express();
 
+/**
+ * This middleware will be execute on all requests, as it has no verb and route.
+ */
+app.use((_, __, next) => {
+  console.log("Generic middleware before routes");
+  next();
+});
+
+/**
+ * This middleware will be execute when /books is requested with the GET verb.
+ * On other routes, this middleware will be ignored
+ */
+app.get("/books", (_, __, next) => {
+  console.log("Specific middleware for GET and route /books");
+  next();
+});
+
 routes(app);
 
 app.use(errorHandling);
