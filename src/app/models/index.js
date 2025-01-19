ba48cd6f0cd96b9b634f2e2e544a57e92956a4a9
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-01-19 12:53:00 -0300

    refact(Validators): Change name file of global string validator

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -1,4 +1,4 @@
-import "./globalValidators/stringGlobalValidator.js";
+import "./globalValidators/globalStringValidator.js";
 
 import Author from "./author/author.js";
 import Book from "./book/book.js";
