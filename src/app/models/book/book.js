2aa4e9e2407af15cabcd776789e6cc507884087e
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-01-19 11:13:00 -0300

    feat(Book): Added validations in mongoose schema for pages and publisher

--- src/app/models/book/book.js
+++ src/app/models/book/book.js
@@ -4,9 +4,20 @@ const bookSchema = mongoose.Schema(
   {
     id: { type: mongoose.Schema.Types.ObjectId },
     title: { type: String, required: [true, "Book title is required"] },
-    publisher: { type: String, required: [true, "Publisher is required"] },
+    publisher: {
+      type: String,
+      required: [true, "Publisher is required"],
+      enum: {
+        values: ["House Code", "O'Reilly"],
+        message: "Publisher does not meet expectations",
+      },
+    },
     price: { type: Number },
-    pages: { type: Number },
+    pages: {
+      type: Number,
+      min: [10, "Pages must be larger than 10"],
+      max: [5000, "Pages must be smaller than 5,000"],
+    },
     author: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "author",
